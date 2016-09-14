75b3dcd0bdfdbc22304f7190bff68e2452adf1be
Edson A. Soares <maxmontenegro@ig.com.br>
2016-09-14 15:23:59 -0300

    Updating the way how the module install the package.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,8 +1,17 @@
 class exim::install {
 
-  exec { "install-exim4":
-      command => "sudo apt-get install --yes --force-yes exim4-daemon-light",
-      path    => [ "/usr/bin", "/bin" ],
+  # exec { "install-exim4":
+  #    command => "sudo apt-get install --yes --force-yes exim4-daemon-light",
+  #    path    => [ "/usr/bin", "/bin" ],
+  # }
+
+  package{ "exim4-daemon-light":
+      ensure => present,
+  } ->
+
+  service{ "exim4":
+      ensure => running,
+      enable => true,
   }
 
 }
